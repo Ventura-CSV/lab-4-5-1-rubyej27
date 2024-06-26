4ba9b6c937bb1127ddc00b65aac1a27d0a1a418c
rubyej27 <ruby_jackson1@my.vcccd.edu>
2024-06-26 11:08:01 -0400

    4-5-1 commit

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ def main():
     Code Your Program here
     ########################################
     """
+    numbers = []
+    count = 0
+    
+    while count < 5:
+        number = random.randint(0,100)
+        numbers.append(number)
+        count += 1
+        
+    total = sum(numbers)
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
